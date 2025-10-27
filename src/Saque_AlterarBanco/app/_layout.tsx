0f7dcaf40369211419e87cd3bc4df8558d1a0c78
Robson Marcolino <166246484+RobsonMarcolino@users.noreply.github.com>
2025-10-26 22:12:40 -0300

    Add files via upload

---
+++ src/Saque_AlterarBanco/app/_layout.tsx
@@ -0,0 +1,21 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    // Stack é o tipo de navegação de "pilha", ideal para fluxos como o de login.
+    <Stack>
+      {/* Registra a tela inicial (login) e remove o cabeçalho */}
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+
+      {/* Registra a tela de cadastro e também remove o cabeçalho */}
+      <Stack.Screen name="cadastro" options={{ headerShown: false }} />
+
+      {/* Registra a tela de recuperação de senha e remove o cabeçalho */}
+      <Stack.Screen name="recuperar-senha" options={{ headerShown: false }} />
+
+       <Stack.Screen name="Saque" options={{ headerShown: false }} />
+       
+      <Stack.Screen name="AlterarConta" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
