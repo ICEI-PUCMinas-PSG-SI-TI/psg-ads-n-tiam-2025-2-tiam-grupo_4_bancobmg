908c698029b88d553605e061d0a73bb5ce6a3679
Marianamartinsmoreira <143458148+Marianamartinsmoreira@users.noreply.github.com>
2025-11-02 11:51:10 -0300

    Delete hooks/use-color-scheme.ts

--- hooks/use-color-scheme.ts
+++
@@ -1,7 +0,0 @@
-import { useColorScheme as _useColorScheme } from 'react-native';
-
-export function useColorScheme(): 'light' | 'dark' {
-  // fallback para 'light' caso seja undefined
-  return _useColorScheme() ?? 'light';
-}
-
